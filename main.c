a0049f101ca13025687e3ea6d36a168362b6abed
Bankhardt Mate <bankhardtmate@gmail.com>
2024-09-22 14:23:32 +0200

    26lines

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rchavez@student.42heilbronn.de <rchavez    +#+  +:+       +#+        */
+/*   By: mbankhar <mbankhar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/23 11:36:45 by rchavez@stu       #+#    #+#             */
-/*   Updated: 2024/09/21 15:23:53 by rchavez@stu      ###   ########.fr       */
+/*   Updated: 2024/09/22 14:22:41 by mbankhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,21 +20,11 @@ int	main(void)
 
 	cub.wall = &obj;
 	cub.p = &p;
-	// || copy_map((char **)cub.map->grid, p) < 0
-	// || player_check((char **)cub.map->grid) < 0
 	if (obj_init(&obj, &cub, "map.cub") < 0)
-	{
-		write(2, "Invalid map\n", 12);
-		return (1);
-	}
-	// printf("%d\n", player_check((char **)cub.map->grid));
-	// printf("%d\n", copy_map((char **)cub.map->grid, p));
+		return (write(2, "Invalid map\n", 12), 1);
 	cub.win = mlx_init(WIDTH, HEIGHT, "Cub3D", 1);
 	if (!cub.win)
-	{
-		write(2, "MLX error\n", 10);
-		return (1);
-	}
+		return (write(2, "MLX error\n", 10), 1);
 	load_textures(cub.wall);
 	mlx_set_window_title(cub.win, "Cub3D");
 	cub.img[0] = mlx_new_image(cub.win, HEIGHT / 4, HEIGHT / 4);
